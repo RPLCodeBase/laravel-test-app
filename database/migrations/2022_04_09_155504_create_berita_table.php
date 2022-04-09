965e16a430c3896470ee8886974e41b68a2f5771
amalia putri <rizmaputri2107@gmail.com>
2022-04-09 22:58:06 +0700

    menambahkan database

---
+++ database/migrations/2022_04_09_155504_create_berita_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->bigIncrements('id_berita');
+            $table->string('id_kategori');
+            $table->string('judul');
+            $table->string('isi_berita');
+            $table->string('gambar');
+            $table->string('waktu');
+            $table->string('counter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita');
+    }
+};
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,4 +1,3 @@
 <x-bootstrap-layout>
-    <h1>Halo</h1>
-    <h1>HaloJuga</h1>
+    
 </x-bootstrap-layout>
